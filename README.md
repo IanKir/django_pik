a0589fbf1ee5644d6d5b16fa2b72edfaf51b3153
IanKir <yankirsh@yandex.ru>
2020-03-24 20:53:15 +0300

    Добавил тесты и подправил README файл

--- README.md
+++ README.md
@@ -1,12 +1,28 @@
-Использовать python3.7\
-Остальные зависимости описаны в requirements.txt\
+Использовать python3.7 \
+Остальные зависимости описаны в requirements.txt
 
-1.Сделать сервис для ведения учёта положенных в доме кирпичей\
-Сущности:\
-Дом (адрес, год постройки)\
-Задание на кладку кирпичей (количество кирпичей)\
-Реализовать три метода:\
-POST /building/ - создать дом\
+Задание:
+
+1.Сделать сервис для ведения учёта положенных в доме кирпичей \
+Сущности: \
+Дом (адрес, год постройки) \
+Задание на кладку кирпичей (количество кирпичей) \
+Реализовать три метода: \
+POST /building/ - создать дом \
 POST /building/{id}/add-bricks/ - положить N кирпичей в дом с id в момент времени T.\
 GET /stats/ - вывести статистику по всем существующим домам - сколько в каждом\
-лежит кирпичей с группировкой по датам. Также необходимо вывести адрес дома.
+лежит кирпичей с группировкой по датам. Также необходимо вывести адрес дома.\
+
+Запуск проекта:
+
+Сделать миграции БД из django_pik\
+python house_building/manage.py makemigrations\
+python house_building/manage.py migrate\
+python house_building/manage.py makemigrations core\
+python house_building/manage.py migrate core
+
+Запустить тесты из django_pik\
+python house_building/manage.oy test core.tests
+
+Запустить приложение из django_pik\
+python houser_building/manage.py runserver
--- house_building/core/tests.py
+++ house_building/core/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from core.models import Task, Building
 
-# Create your tests here.
+
+class TaskTestCase(TestCase):
+    def setUp(self):
+        building = Building.objects.create()
+        Task.objects.create(building_number=building, bricks_quantity=500)
+        Task.objects.create(building_number=building, bricks_quantity=1000)
+
+    def test_task_creating(self):
+        task_1 = Task.objects.get(bricks_quantity=500)
+        task_2 = Task.objects.get(bricks_quantity=1000)
+        self.assertEqual(task_1.bricks_quantity, 500)
+        self.assertEqual(task_2.bricks_quantity, 1000)
+
+
+class BuildingTestCase(TestCase):
+    def setUp(self):
+        building_1 = Building.objects.create(title='Дом на Театральной')
+        building_2 = Building.objects.create(title='Дом на Пушкинской')
+        Task.objects.create(building_number=building_1, bricks_quantity=550)
+        Task.objects.create(building_number=building_1, bricks_quantity=116)
+        Task.objects.create(building_number=building_2, bricks_quantity=300)
+        Task.objects.create(building_number=building_2, bricks_quantity=700)
+
+    def test_building_get_bricks_amount(self):
+        building_1_test = Building.objects.get(title='Дом на Театральной')
+        building_2_test = Building.objects.get(title='Дом на Пушкинской')
+        self.assertEqual(building_1_test.get_bricks_amount(), 666)
+        self.assertEqual(building_2_test.get_bricks_amount(), 1000)
